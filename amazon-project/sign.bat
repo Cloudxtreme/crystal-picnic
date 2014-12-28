2b732c2fbc5b7e20e32591e7d1ebe89c83d60f79
Trent Gamblin <trent@nooskewl.ca>
2014-12-27 17:23:04 -0700

    Build with sources in place on Android*. Sign with keystore in ..\..

--- amazon-project/sign.bat
+++ amazon-project/sign.bat
@@ -1,6 +1,6 @@
 del %1
 copy bin\crystalpicnic-project-release-unsigned.apk tmp.apk
-c:\Progra~1\Java\jdk1.8.0_05\bin\jarsigner.exe -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore my-release-key.keystore tmp.apk Nooskewl
+c:\Progra~1\Java\jdk1.8.0_05\bin\jarsigner.exe -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore ..\..\my-release-key.keystore tmp.apk Nooskewl
 c:\Progra~1\Java\jdk1.8.0_05\bin\jarsigner.exe -verify tmp.apk
 c:\Users\Trent\AppData\Local\Android\android-sdk\build-tools\19.1.0\zipalign.exe -v 4 tmp.apk %1
 del tmp.apk
