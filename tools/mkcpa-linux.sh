78cf8a45eab01bccf7f10296260e836a5f6f917d
Rémi Verschelde <rverschelde@gmail.com>
2015-08-15 11:01:50 +0200

    Better document the need for fonts and soundfont
    Also had a check in mkcpa-linux.sh to abort the creation of the cpa if those files are missing.
    Fixes #5

--- tools/mkcpa-linux.sh
+++ tools/mkcpa-linux.sh
@@ -5,6 +5,12 @@ if [ ! -d data ]; then
   exit 1
 fi
 
+if [ ! -e data/fonts/heavy.ttf -o ! -e data/fonts/light.ttf -o ! -e data/music/retro.sf2 ]; then
+  echo "Crystal Picnic requires two fonts and one MIDI soundfont which are not available in the git repo, and at least one of them seems missing."
+  echo "Check data/Notice.txt for details. Aborting, as the resulting data.cpa would be incomplete."
+  exit 2
+fi
+
 ROOT=$(pwd)
 mkdir -p ${ROOT}/build
 
