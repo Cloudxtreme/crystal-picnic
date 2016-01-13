9d9b380bd483140cc4e06993589d7e2c5020132b
Trent Gamblin <trent@nooskewl.ca>
2016-01-13 05:17:49 -0700

    Add project for building Android demo version

---
+++ android-demo-project/jni/Application.mk
@@ -0,0 +1,6 @@
+APP_OPTIM := release
+APP_PLATFORM := android-9
+APP_ABI := armeabi-v7a x86
+APP_MODULES := liballegro_monolith-prebuilt libbass-prebuilt libbassmidi-prebuilt crystalpicnic
+APP_STL := gnustl_static
+APP_CPPFLAGS += -fexceptions -frtti
---
+++ android-demo-project/src/ca/nooskewl/crystalpicnicdemo/MyBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package ca.nooskewl.crystalpicnicdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.BroadcastReceiver;
+
+public class MyBroadcastReceiver extends BroadcastReceiver
+{
+	native static void pauseSound();
+	native static void resumeSound();
+
+	public void onReceive(Context context, Intent intent)
+	{
+		if (intent.getAction() == "android.intent.action.DREAMING_STARTED") {
+			pauseSound();
+		}
+		else if (intent.getAction() == "android.intent.action.DREAMING_STOPPED") {
+			resumeSound();
+		}
+	}
+}
+
+
