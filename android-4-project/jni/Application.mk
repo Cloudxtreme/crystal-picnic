77cd347facfe5b97375cab6bc3338f4757cee03f
Trent Gamblin <trent@nooskewl.ca>
2016-01-13 05:16:03 -0700

    Remove Android 4 project - main project now builds all versions

--- android-4-project/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-APP_OPTIM := release
-APP_PLATFORM := android-16
-APP_ABI := armeabi-v7a
-APP_MODULES := liballegro_monolith-prebuilt libbass-prebuilt libbassmidi-prebuilt crystalpicnic
-APP_STL := gnustl_static
-APP_CPPFLAGS += -fexceptions -frtti
