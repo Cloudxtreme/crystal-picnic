bb4053fcf9bc81679d54e5d9ce4fea8f022b4a0e
Trent Gamblin <trent@nooskewl.ca>
2016-01-13 05:16:34 -0700

    Android project now builds for Android 9 up

--- android-project/jni/Application.mk
+++ android-project/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_OPTIM := release
-APP_PLATFORM := android-21
+APP_PLATFORM := android-9
 APP_ABI := armeabi-v7a x86
 APP_MODULES := liballegro_monolith-prebuilt libbass-prebuilt libbassmidi-prebuilt crystalpicnic
 APP_STL := gnustl_static
